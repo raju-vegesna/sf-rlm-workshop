b3fd965ecc2676de18cc17452933a4fb0752ad48
raju.vegesna <>
2024-08-26 19:36:26 +0100

    quick fix.

--- build/auth_validate.sh
+++ build/auth_validate.sh
@@ -36,8 +36,8 @@ esac
 
 # Authenticate to salesforce
 echo "Authenticating with ... " $SFDC_USER 
-# sfdx force:auth:jwt:grant --client-id $CLIENT_ID --jwtkey-file keys/server.key --username $USER --instance-url $SFDC_INSTANCE_URL
-sf org login jwt --client-id=$SFDC_CLIENTID --jwt-key-file=keys/server.key --username=$SFDC_USER --instance-url=$SFDC_INSTANCE_URL
+sfdx force:auth:jwt:grant --client-id $CLIENT_ID --jwtkey-file keys/server.key --username $USER --instance-url $SFDC_INSTANCE_URL
+# sf org login jwt --client-id=$SFDC_CLIENTID --jwt-key-file=keys/server.key --username=$SFDC_USER --instance-url=$SFDC_INSTANCE_URL
 
 #Validate against org & run local tests
 echo "Validate against ORG & run local tests..." 
